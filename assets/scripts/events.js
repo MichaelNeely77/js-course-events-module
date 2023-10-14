058ba61cccffc5dcb5f21fdf5fc7348e76be065a
Michael Neely <michael@michaelneely.net>
2023-10-14 09:52:35 -0400

    test event.preventDefault() and got a better understanding of that.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -44,3 +44,12 @@ const anotherClickHandler = () => {
 // }
 //
 // window.addEventListener('scroll', scrollHandler);
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
+
+
+})
